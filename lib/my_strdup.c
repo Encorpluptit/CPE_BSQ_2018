f3745ec1ba1fac2e3203ce0acdf73d37fb347173
Bernard Damien <BDamien@localhost.localdomain>
2019-04-15 22:21:05 +0200

    [ADD] adding my_strdup, strlen and strcpy for function used in library

---
+++ lib/my_strdup.c
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2019
+** my_strdup
+** File description:
+** alllocate memory and copy a string given as a argument
+*/
+#include <stdlib.h>
+int my_strlen(char const *str);
+char *my_strcpy(char *dest, char const *src);
+
+char *my_strdup(char const *src)
+{
+    int src_len = my_strlen(src);
+    char *dest;
+
+    dest = malloc(sizeof(char) * src_len);
+    dest = my_strcpy(dest, src);
+    dest[src_len] = '\0';
+    return dest;
+}
