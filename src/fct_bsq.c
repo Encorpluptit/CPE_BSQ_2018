e195b0a217689550cce1bc7f374e018fdd3b5053
Bernard Damien <BDamien@localhost.localdomain>
2019-04-16 11:22:12 +0200

    [FIX] Fixing condition with calcul (adding if we are on a dot)

--- src/fct_bsq.c
+++ src/fct_bsq.c
@@ -29,20 +29,17 @@ static short make_calcul(short left_case, short up_case, short upleft_case)
         return (upleft_case + 1);
 }
 
-static int **maximum(int **ptri, int i, short arr)
+static void maximum(int *ptri, int i, short arr)
 {
     static short maximum = 0;
 
     if (arr > maximum) {
         maximum = arr;
-        **ptri = i;
-        return (ptri);
+        *ptri = i;
     }
-    else
-        return (ptri);
 }
 
-static short *fill_array(short *arr, char const *str, int nb_column, int **ptri)
+static short *fill_array(short *arr, char const *str, int nb_column, int *ptri)
 {
     int i = 0;
     int j = 1;
@@ -53,10 +50,10 @@ static short *fill_array(short *arr, char const *str, int nb_column, int **ptri)
             write(2, "Invalid Map, please use a valid file.\n", 38);
             return NULL;
         }
-        if (i > nb_column && (i % (nb_column * j)) != 0) {
+        if (str[i] == '.' && i > nb_column && (i % (nb_column * j)) != 0) {
             arr[i] = make_calcul(arr[i - 1], arr[i - nb_column],
                                 arr[i - nb_column - 1]);
-            ptri = maximum(ptri, i, arr[i]);
+            maximum(ptri, i, arr[i]);
         }
         (i != nb_column) && (i % (nb_column * j)) == 0 ? j++ : 0;
         str[i] == 'o' ? arr[i] = 0 : 0;
@@ -90,13 +87,12 @@ int fct_bsq(char *main_str)
     int nb_lign = wipe_first_line(&main_str);
     int nb_column = get_nb_column(main_str);
     short *arr = malloc(sizeof(short) * (nb_lign * nb_column + 1));
-    int index = 0;
-    int *ptri = &index;
+    int ptri = 0;
 
     arr = fill_array(arr, main_str, nb_column, &ptri);
     if (arr == NULL)
         return (84);
-    print_bsq(main_str, index, nb_column, arr[index]);
+    print_bsq(main_str, ptri, nb_column, arr[ptri]);
     free(arr);
     return (0);
 }
