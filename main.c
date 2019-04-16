2edacca940a18fca1e882c9fcb60a74d52c94cdb
Bernard Damien <BDamien@localhost.localdomain>
2019-04-16 14:40:28 +0200

    [FIX] Fixing error handeling in main if nb of argument is different of 2

--- main.c
+++ main.c
@@ -14,6 +14,8 @@ int main(int ac, char **av)
 {
     char *main_str;
 
+    if (ac != 2)
+        return (84);
     main_str = get_main_str(av[1]);
     if (main_str == NULL) {
         write(2, "Invalid Map, please use a valid file.\n", 38);
